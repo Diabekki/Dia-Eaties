2966ab9341816f9f4ecef024af16243be7a393be
Diabekki <bekkitubridy@gmail.com>
2021-10-28 17:16:34 +0000

    Render forms.py and made comment section

--- diaeatiesblog/urls.py
+++ diaeatiesblog/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.ListPost.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
 ]
+
